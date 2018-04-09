9be320f5ad4fa91018578f60ecae80db00d28982
mali_server <mali1711@163.com>
2018-04-09 16:22:07 +0800

    修改数据库

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '123.207.22.164',
     // 数据库名
     'database'        => 'xianmaidang',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => '123456',
+    'password'        => 'Abc123456789@',
     // 端口
     'hostport'        => '',
     // 连接dsn
